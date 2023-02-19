7915a368c2036d4dd81a84a735a4cb65a7e9226e
Khalid Ahmed <khhaaaliid@gmail.com>
2023-02-19 16:10:54 -0500

    test

--- src/main/java/ShelterTest.java
+++ src/main/java/ShelterTest.java
@@ -1,40 +1,37 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.Test;
+public class ShelterTest {
+    Shelter underTest = new Shelter();
+    VirtualPet petOne = new VirtualPet("petOne","lame");
+    VirtualPet petTwo = new VirtualPet("petTwo","whatisks'dkvs/v");
+    VirtualPet petThree = new VirtualPet("petThree", "him");
 
-class ShelterTest {
+    underTest.
+    underTest.put(petOne.getName(), petOne);
+    underTest.put(petTwo.getName(), petTwo);
+    underTest.put(petThree.getName(), petThree);
 
-    Shelter AllPets = new Shelter();
 
-    @Test
-    public void playWithAllPets() {
-        Shelter shelter = new Shelter();
-        VirtualPet petOne = new VirtualPet("petOne", "lame");
-        VirtualPet petTwo = new VirtualPet("petTwo", "whatisks'dkvs/v");
-        VirtualPet petThree = new VirtualPet("petThree", "him");
-        shelter.addToShelter(petOne);
-        shelter.addToShelter(petTwo);
-        shelter.addToShelter(petThree);
-        shelter.playWithAllPets();
-    }
+    //    @Test
+//    public void shouldWaterAllPets() {
+//        underTest.allPetsDrink();
+//        assertEquals(4,petOne.getThirst());
+//        assertEquals(4,petTwo.getThirst());
+//        assertEquals(4,petThree.getThirst());
+//    }
 
-    @Test
-    public void tickAllPets() {
-        Shelter shelter = new Shelter();
-        VirtualPet petOne = new VirtualPet("petOne", "lame");
-        VirtualPet petTwo = new VirtualPet("petTwo", "whatisks'dkvs/v");
-        VirtualPet petThree = new VirtualPet("petThree", "him");
-        shelter.addToShelter(petOne);
-        shelter.addToShelter(petTwo);
-        shelter.addToShelter(petThree);
-        assertEquals(petOne.getHealth(), 9);
-        assertEquals(petOne.getHealth(), 9);
-        assertEquals(petOne.getHealth(), 9);
-        for (VirtualPet pet : shelter.shelter.values()) {
-            pet.tick();
-            System.out.println("Ran tick on " + pet.getName());
-        }
-        assertEquals(petOne.getHealth(), 8);
-        assertEquals(petTwo.getHealth(), 8);
-        assertEquals(petThree.getHealth(), 8);
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/ShelterTest.java
@@ -0,0 +1,7 @@
+import org.junit.jupiter.api.Test;
+
+public class ShelterTest {
+    @Test
+    Shelter shelter = new Sheq
+
+}
